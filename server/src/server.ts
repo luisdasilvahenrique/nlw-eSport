61207b9ff7c0dac2c4f0b861d0dd58a3a6114766
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-09-11 23:01:56 -0300

    :tada: initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        {id: 1, name: 'John'},
+        {id: 2, name: 'Luis'},
+        {id: 3, name: 'Pedro'},
+        {id: 4, name: 'Uanderson'}
+    ]);
+});
+
+app.listen(3333);

d461657f4c54cc7341f05731f1938acde9c60f17
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-09-13 14:54:26 -0300

    loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,32 @@
-import { View } from "react-native";
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
 
+import { Home } from './src/screens/Home';
 import { Background } from "./src/Components/Background";
+import { Loading } from './src/Components/Loading';
 
 export default function App() {
+ const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
+      <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      {!fontsLoader ? <Home/> : <Loading/>}
 
     </Background>
   );

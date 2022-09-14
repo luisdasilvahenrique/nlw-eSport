204d72684ab3125f61b750579175879c5cf8d1ae
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-09-14 14:27:22 -0300

    Criando api

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,34 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
-    return response.json([
-        {id: 1, name: 'John'},
-        {id: 2, name: 'Luis'},
-        {id: 3, name: 'Pedro'},
-        {id: 4, name: 'Uanderson'},
-        {id: 5, name: 'Aparecida'},
+// List games
+app.get('/games', (req, res) => {
+    return res.json([])
+})
+// Creat ads
+app.post('/ads', (req, res) => {
+    return res.status(201).json([])
+})
+// List discord by ads
+app.get('/games/:id/discord', (req, res) => {
+    //  const adsID = req.params.id;
+
+    return res.json([]);
+
+});
+// List ads by games
+app.get('/games/:id/ads', (req, res) => {
+    //  const gameId = req.params.id; 
+
+    return res.json([
+        { id: 1, name: 'John' },
+        { id: 2, name: 'Luis' },
+        { id: 3, name: 'Pedro' },
+        { id: 4, name: 'Uanderson' },
+        { id: 5, name: 'Aparecida' },
     ]);
 });
+
+
 
 app.listen(3333);
